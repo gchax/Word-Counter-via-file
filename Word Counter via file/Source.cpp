bdffd38c95bb1e04a9e06f9a1c1525f5a61be8f1
Chanon <pcg.mega@gmail.com>
2020-11-01 03:28:07 +0700

    Add project files.

---
+++ Word Counter via file/Source.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void printFile(char*, FILE*);
+int countWords(char, FILE*);
+
+int main()
+{
+	FILE* f1, * f2;
+	char readc = {}, read[999];
+	int n, count = 0;
+
+	f1 = fopen("file.txt", "r");
+	printFile(&read[0], f1);
+	fclose(f1);
+
+	f2 = fopen("file.txt", "r");
+	n = countWords(readc, f2);
+	printf("\n\nThere are %d words in this file.\n", n);
+	fclose(f2);
+	return 0;
+}
+
+void printFile(char* s, FILE* ptr)
+{
+	while (!feof(ptr))
+	{
+		fgets(s, 999, ptr);
+		printf("%s", s);
+	}
+}
+
+int countWords(char c, FILE* ptr)
+{
+	int state = 0, n = 0;
+	while ((c = fgetc(ptr)) != EOF)
+	{
+		if (c == ' ' || c == '\n' || c == '\t' || c == '\0') state = 0;
+		else if (state == 0)
+		{
+			state = 1;
+			n++;
+		}
+	}
+	return n;
+}
